04e24590c40821854815e75777a9eb709c97a2c4
Steven Carmichael <blekenbleu@gmail.com>
2026-02-17 09:07:19 -0500

    ChannelEventBus CEB = new(); fails

--- Channel.cs
+++ Channel.cs
@@ -1,16 +1,21 @@
 // https://medium.com/@abhirajgawai/c-channels-explained-from-producer-consumer-basics-to-high-performance-net-systems-f8ab610c0639
 
-using System.Threading.Channels
+using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+// https://www.csharptutorial.net/csharp-tutorial/csharp-record/
+
 
 namespace blekenbleu.OpenKneeboard_SimHub_plugin_menu
 {
-	// https://www.csharptutorial.net/csharp-tutorial/csharp-record/
-	public record MidiChannelEvent( uint32 Payload );	// pass from Naudio MIDI device input events
+	internal class MidiChannelEvent
+	{ internal UInt32 Payload; } // pass from Naudio MIDI device input events
 
 	public sealed class ChannelEventBus : IDisposable
-    {
+	{
 		private readonly Channel<MidiChannelEvent> _channel;
-		
+
 		public ChannelEventBus()
 		{
 			// Multiple producers, single consumer 
@@ -18,48 +23,83 @@ namespace blekenbleu.OpenKneeboard_SimHub_plugin_menu
 				// Bounded to 100. If full, drop oldest.
 				new BoundedChannelOptions(100)
 				{
-				    SingleReader = true,  // Optimization hint
-		    		SingleWriter = false  // Multiple producers
-			    	FullMode = BoundedChannelFullMode.DropOldest
+					SingleReader = true,  // Optimization hint
+					SingleWriter = false,  // Multiple producers
+
+					FullMode = BoundedChannelFullMode.DropOldest
 				},
-				static void (MidiChannelEvent dropped) +>
-   				{
-					OKSHmenu.Info($"MidiChannelEvent Dropped: {dropped}");
-				}
-			}
-		);
-
-		public ChannelReader<MidiChannelEvent> Reader => _channel.Reader;
-
-		while (await Reader.WaitToReadAsync(token))
-		{
-    		while (reader.TryRead(out var item))
-    		{
-        		Process(item);
-    		}
+				(MidiChannelEvent dropped) =>
+					   OKSHmenu.Info($"MidiChannelEvent Dropped: {dropped}")
+			);
 		}
-	}			class ChannelEventBus
 
-/*
-	public class WebhookProcessorService : BackgroundService
-	{
-    	private readonly IEventBus _eventBus;
+		internal ChannelReader<MidiChannelEvent> Reader => _channel.Reader;
+		internal ChannelWriter<MidiChannelEvent> Writer => _channel.Writer;
 
-    	public WebhookProcessorService(IEventBus eventBus)
-    	{
-        	_eventBus = eventBus;
-    	}
-	}
- */
+		internal async Task ReadAsync()
+		{
+			while (await Reader.WaitToReadAsync())
+    			while (Reader.TryRead(out MidiChannelEvent item))
+					MIDI.Process(item);
+		}
+
+        public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}   //		class ChannelEventBus
+
+	/*
+		public class WebhookProcessorService : BackgroundService
+		{
+			private readonly IEventBus _eventBus;
+
+			public WebhookProcessorService(IEventBus eventBus)
+			{
+				_eventBus = eventBus;
+			}
+		}
+	 */
 	internal partial class MIDI
 	{
-		void Enqueue (uint32 inDevice, uint32 payload)
-		// https://learn.microsoft.com/en-us/dotnet/core/extensions/channels#producer-patterns
+		static readonly ChannelEventBus CEB = new();
+		
+        internal void Enqueue(UInt32 inDevice, uint payload)
+        // https://learn.microsoft.com/en-us/dotnet/core/extensions/channels#producer-patterns
+        {
+            payload |= (inDevice << 24);
+            // Fire-and-forget
+            if (CEB.Writer.TryWrite(new MidiChannelEvent() { Payload = payload }))
+                return;
+            OKSHmenu.Info(lMidiIn[3].id + ".Enqueue(" + payload.ToString() + ") failed");
+        }
+
+        // Handle Control Change (0xB0), Patch Change (0xC0) and Bank Select (0xB0) channel messages
+        // https://github.com/naudio/NAudio/blob/master/NAudio.Midi/Midi/MidiEvent.cs#L24
+        // https://www.hobbytronics.co.uk/wp-content/uploads/2023/07/9_MIDI_code.pdf
+        internal static void Process(MidiChannelEvent item)
 		{
-			uint32 payload |= (inDevice << 24);
-			// Fire-and-forget
-			if (!ChannelWriter<MidiChannelEvent>.TryWrite(new MidiChannelEvent(payload)))
-				OKSHmenu.Info(lMidiIn[3].id + ".Enqueue(" + e.MidiEvent.ToString() + ") failed");
+			// NAudio bytes are reversed from e.g. MidiView and WetDry:  Status byte is least significant..
+			UInt32 RawMessage = item.Payload;
+
+			var c = 0x0F & RawMessage;            // 0x0F & e.RawMessage
+			var d1 = (RawMessage >> 8) & 0xff;
+			var d2 = (RawMessage >> 16) & 0xff;
+			var dev = (RawMessage >> 24) & 0x0f;
+			switch (0xF0 & RawMessage)  // 0x80 <= (0xF0 & e.RawMessage) < 0xF0
+			{
+				case 0x80:
+				case 0x90:
+				case 0xA0:
+					OKSHmenu.Info($"Process({RawMessage:X}) ignored");
+					break;
+				case 0xF0:
+					OKSHmenu.Info($"Process({RawMessage:X}) ignored");
+					break;
+				default:
+					OKSHmenu.Info($"Process({RawMessage:X}) to do");
+					break;
+			}
 		}
 	}
 }
